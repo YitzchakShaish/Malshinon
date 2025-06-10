bdb5d3cf986bb4c5758f083f391d504e74a35104
yitzchak shaish <y350380350@gmail.com>
2025-06-10 12:14:42 +0300

    Added method InsertNameAndMessage in class Report

--- Report.cs
+++ Report.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using static System.Net.Mime.MediaTypeNames;
 
 
 namespace Malshinon
 {
     internal class Report
     {
-        public void InsertName()
+        public int InsertName()
         {
             Console.WriteLine("enter your fool name; ");
             string[] foolName = Console.ReadLine().Split();
@@ -28,24 +30,31 @@ namespace Malshinon
 
                 People newP = new People(first_name, last_name, secret_code);
                 rd.AddPeople(newP);
+                id = rd.RequestingIDFromPeopleTable(first_name);
                 Console.WriteLine("A new people has been created successfully.");
-                Console.WriteLine($"Your first name is:{first_name} Your ID is: {rd.RequestingIDFromPeopleTable(first_name)}");
+                Console.WriteLine($"Your first name is:{first_name} Your ID is: {id}");
             }
             else
             {
+                id = rd.RequestingIDFromPeopleTable(first_name);
+
                 Console.WriteLine("The people is already in the database.");
-                Console.WriteLine($"Your first name is:{first_name} Your ID is: {rd.RequestingIDFromPeopleTable(first_name)}");
+                Console.WriteLine($"Your first name is:{first_name} Your ID is: {id}");
 
             }
+            return id;
+
         }
 
-        public void InsertMessage()
+        public Dictionary<int, string> InsertMessage()
         {
             Console.WriteLine("enter your message; ");
-            string[] message = Console.ReadLine().Split(' ');
+            string message = Console.ReadLine();
+            string[] arrMessage = message.Split(' ');
             string first_name = null;
             string last_name = null;
-            foreach (string word in message)
+            Dictionary<int, string> dictMessage = new Dictionary<int, string>();
+            foreach (string word in arrMessage)
             {
                 if (char.IsUpper(word[0]) & first_name == null)
                 { 
@@ -69,15 +78,40 @@ namespace Malshinon
 
                 People newP = new People(first_name, last_name, secret_code);
                 rd.AddPeople(newP);
+                id = rd.RequestingIDFromPeopleTable(first_name);
                 Console.WriteLine("A new people has been created successfully.");
-                Console.WriteLine($"Your first name is:{first_name} Your ID is: {rd.RequestingIDFromPeopleTable(first_name)}");
+                Console.WriteLine($"Your first name is:{first_name} Your ID is: {id}");
             }
             else
             {
-                Console.WriteLine("The people is already in the database.");
-                Console.WriteLine($"Your first name is:{first_name} Your ID is: {rd.RequestingIDFromPeopleTable(first_name)}");
-            }
-        }
+                id = rd.RequestingIDFromPeopleTable(first_name);
 
+                Console.WriteLine("The people is already in the database.");
+                Console.WriteLine($"Your first name is:{first_name} Your ID is: {id}");
+            }
+            dictMessage[id] = message;
+            return dictMessage;
+        }
+        public void InsertNameAndMessage()
+        {
+            int reporter_id = InsertName();
+            Dictionary<int, string> dictMessage = InsertMessage();
+            int target_id = 0;
+            string text ="";
+            if (dictMessage.Count == 1)
+            {
+                var pair = dictMessage.First();
+                target_id = pair.Key;
+                text = pair.Value;
+            }
+            Console.WriteLine($"reporter_id: {reporter_id}");
+            Console.WriteLine($"target_id: {target_id}");
+            Console.WriteLine($"text: {text}");
+
+        
+
+
+
+    }
     }
 }
