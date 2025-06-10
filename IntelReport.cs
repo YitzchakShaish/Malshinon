8473fa42a01405f618de09f40105f15980481b9f
yitzchak shaish <y350380350@gmail.com>
2025-06-10 12:24:06 +0300

    Added class IntelReport

---
+++ IntelReport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon
+{
+    internal class IntelReport
+    {
+        int id;
+        int reporter_id;
+        int target_id;
+        string text;
+        DateTime timestamp;
+
+        public IntelReport(int reporter_id, int target_id, string text)
+        {
+            this.reporter_id = reporter_id;
+            this.target_id = target_id;
+            this.text = text;
+        }
+    }
+}
