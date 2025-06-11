0451dfc266e8d5d24fad7ba334414028185bbe29
yitzchak shaish <y350380350@gmail.com>
2025-06-11 21:19:35 +0300

    Create a MODALS folder with the classes that define a type

---
+++ Models/IntelReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon
+{
+    internal class IntelReport
+    {
+        public int id;
+        public int reporter_id;
+        public int target_id;
+        public string text;
+        public bool is_dangerous;
+        public DateTime timestamp;
+
+        public IntelReport(int reporter_id, int target_id, string text)
+        {
+            this.reporter_id = reporter_id;
+            this.target_id = target_id;
+            this.text = text;
+        }
+       
+    }
+}
