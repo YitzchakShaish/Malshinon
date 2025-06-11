d5330d84f159d5b9116a54e45e2051b695ccc67d
yitzchak shaish <y350380350@gmail.com>
2025-06-11 11:58:57 +0300

    Added  metod CreateSecretCode

--- People.cs
+++ People.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace Malshinon
 {
-    //public enum PersonType
-    //{
-    //    Reporter,
-    //    Target,
-    //    Both,
-    //    PotentialAgent
-    //}
+    public enum PersonType
+    {
+        reporter,
+        target,
+        both,
+        potential_agent
+    }
     internal class People
     {
      public   int id;
      public   string first_name;
      public   string last_name;
      public   string secret_code;
-     public   string type_poeple;
+     public   PersonType type_poeple;
      public   int num_reports = 0;
      public   int num_mentions = 0;
     
@@ -28,13 +28,6 @@ namespace Malshinon
             this.first_name = first_name;
             this.last_name = last_name;
             this.secret_code = secret_code;
-            
-            //if (!Enum.TryParse<PersonType>(type_poeple, true, out var parsedType))
-            //{
-            //    throw new ArgumentException("Invalid person type");
-            //}
-
-            //this.type_poeple = parsedType;
         }
 
         public People(string first_name, string last_name)
@@ -42,9 +35,12 @@ namespace Malshinon
             this.first_name = first_name;
             this.last_name = last_name;
         }
-
-
-
+        public void CreateSecretCode(string firstName)
+        {
+            string newSecretCode  = GenretrRandom.GenerateCode(8,firstName);
+            Console.WriteLine("secret_code=: " + secret_code);
+            secret_code = newSecretCode;
+        }
         public override string ToString()
         {
             return $"id: {id}, first_name: {first_name}, last_name: {last_name}, type_poeple: {type_poeple}, secret_code: {secret_code}, num_reports {num_reports}, num_mentions: {num_mentions} ";
