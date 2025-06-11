b9ffc65260b8867c60bad8a15b7c26a827e5a688
yitzchak shaish <y350380350@gmail.com>
2025-06-11 12:04:49 +0300

    Introduced and implemented the new methods for creating secret code (and changing the names of methods).

--- Report.cs
+++ Report.cs
@@ -18,25 +18,24 @@ namespace Malshinon
             string[] foolName = Console.ReadLine().Split();
             string first_name = foolName[0];
             string last_name = foolName[1];
-            //Console.WriteLine($"firstname: {first_name}, lastname: {last_name}");
             ReportDal rd = new ReportDal();
 
-            int id = rd.RequestingIDFromPeopleTable(first_name);
+            int id = rd.GetPeopleID(first_name);
             if (id == -1)
             {
-                Random rnd = new Random();
-                string secret_code = GenretrRandom.GenerateCode(8);
-                Console.WriteLine("secret_code=: " + secret_code);
 
-                People newP = new People(first_name, last_name, secret_code);
+
+                People newP = new People(first_name, last_name);
+                newP.CreateSecretCode(first_name);
                 rd.AddPeople(newP);
-                id = rd.RequestingIDFromPeopleTable(first_name);
+
+                id = rd.GetPeopleID(first_name);
                 Console.WriteLine("A new people has been created successfully.");
                 Console.WriteLine($"Your first name is:{first_name} Your ID is: {id}");
             }
             else
             {
-                id = rd.RequestingIDFromPeopleTable(first_name);
+                id = rd.GetPeopleID(first_name);
 
                 Console.WriteLine("The people is already in the database.");
                 Console.WriteLine($"Your first name is:{first_name} Your ID is: {id}");
@@ -50,41 +49,36 @@ namespace Malshinon
         {
             Console.WriteLine("enter your message; ");
             string message = Console.ReadLine();
-            string[] arrMessage = message.Split(' ');
+            string[] arrMessage = message.Split();
             string first_name = null;
             string last_name = null;
             Dictionary<int, string> dictMessage = new Dictionary<int, string>();
             foreach (string word in arrMessage)
             {
                 if (char.IsUpper(word[0]) & first_name == null)
-                { 
+                {
                     first_name = word;
                 }
                 else if (char.IsUpper(word[0]))
-                { 
+                {
                     last_name = word;
                     break;
                 }
             }
-            Console.WriteLine($"firstname: {first_name}, lastname: {last_name}");
             ReportDal rd = new ReportDal();
-
-            int id = rd.RequestingIDFromPeopleTable(first_name);
+            int id = rd.GetPeopleID(first_name);
             if (id == -1)
             {
-                Random rnd = new Random();
-                string secret_code = GenretrRandom.GenerateCode(8);
-                Console.WriteLine("secret_code=: " + secret_code);
-
-                People newP = new People(first_name, last_name, secret_code);
+                People newP = new People(first_name, last_name);
+                newP.CreateSecretCode(first_name);
                 rd.AddPeople(newP);
-                id = rd.RequestingIDFromPeopleTable(first_name);
+                id = rd.GetPeopleID(first_name);
                 Console.WriteLine("A new people has been created successfully.");
                 Console.WriteLine($"Your first name is:{first_name} Your ID is: {id}");
             }
             else
             {
-                id = rd.RequestingIDFromPeopleTable(first_name);
+                id = rd.GetPeopleID(first_name);
 
                 Console.WriteLine("The people is already in the database.");
                 Console.WriteLine($"Your first name is:{first_name} Your ID is: {id}");
@@ -98,19 +92,13 @@ namespace Malshinon
             Dictionary<int, string> dictMessage = InsertMessage();
             ReportDal rd = new ReportDal();
 
-            int target_id = 0;
-            string text ="";
-            if (dictMessage.Count == 1)
-            {
-                var pair = dictMessage.First();
-                target_id = pair.Key;
-                text = pair.Value;
-            }
+
+            var pair = dictMessage.First();
+            int target_id = pair.Key;
+            string text = pair.Value;
+
             IntelReport newIntelReport = new IntelReport(reporter_id, target_id, text);
             rd.AddIntelReport(newIntelReport);
-            rd.UpdatedNumReports(reporter_id);
-            rd.UpdatedNumMentions(target_id);
-            rd.TestingPotentialAgent(reporter_id);
-    }
+             }
     }
 }
